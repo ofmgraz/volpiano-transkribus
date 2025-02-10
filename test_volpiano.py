bb75f35402fcfbc30a9e2394a77329b07c363b52
Johanna Unterholzner <johannamaria.unterholzner@oeaw.ac.at>
2025-02-10 11:43:24 +0100

    Update CLEFS, determine_clef, token2volp: Kustos, b

--- test_volpiano.py
+++ test_volpiano.py
@@ -1,17 +1,26 @@
-from volpiano import determine_clef, volp
+from volpiano import determine_clef, volp, token2volp
 
 
 def test_simple():
     assert determine_clef("c2") == "c2"
-
-
-def test_simple_2():
     assert determine_clef("c3") == "c3"
 
 
-# def test_double():
-#     assert determine_clef("c4/f2") == "c4"
+def test_simple_2():
+    assert determine_clef("g4/c2") == "c2"
+    assert determine_clef("c4/f2") in ("c4", "f2")
+    assert determine_clef("[c4]") == "c4"
+
+
+def test_token2volp():
+    assert token2volp("f2", "cu_z2") == "--G"
+    assert token2volp("c3", "cu_z2") == "--J"
 
 
 def test_excel_example():
-    assert volp("g4/c2 z2 , z2 , l2 , z2 l2 l2 z1 , l2 , z2 l3 z2 l2 z2 , z2 z3 z2 l3 z2 l2 z2 l2 L z2 , z3 l3 z3 l4 L l4 z4 , z2 z2 z3 z3 l3") == "1-l-l-k-lkkj-k-lmlkl-lnlmlklk-3-lnmno-3-op-jjllk"
+    assert volp("g4/c2 z2 , z2 , l2 , z2 l2 l2 z1 , l2 , z2 l3 z2 l2 z2 , z2 z3 z2 l3 z2 l2 z2 l2 L z2 , z3 l3 z3 l4 L l4 z4 , z2 z2 z3 z3 l3") == "1-l-l-k-lkkj-k-lmlkl-lnlmlklk-3-l-nmno-3-op-llnnm"
+
+
+def test_b2volp(): 
+    assert volp("c4 l3 , l3 bz3 , z2 cu_z2") == "1-h-hi-g--G"
+    assert volp("c4 l3 , l3 bz3 nz3 , z2 cu_z2") == "1-h-hiI-g--G"
--- volpiano.py
+++ volpiano.py
@@ -10,26 +10,6 @@ NS_MAP = {"p": P_NS}
 INFILE = "D-MbsClm2766_Seite_010.xml"
 OUTFILE = "D-MbsClm2766_Seite_010.json"
 
-# # c4 z1, z2 , z2
-# f(c4, 10) => 9
-# f(c4, 5) => 4
-#
-# f(c2, 7) => 10
-# f(c2, 6) => 9
-# f(c2, 10) => 13
-# f(c2, 11) => 14
-#
-# f(c1, 5) => 10
-# f(c1, 4) => 9
-# f(c1, 8) => 13
-# f(c1, 9) => 14
-#
-# # ???
-# f(c1, 7) => 5
-# f(c1, 6) => 4
-# f(c1, 10) => 8
-# f(c1, 11) => 9
-
 VOLP2NUM = {
     "a": 0,
     "b": 1,
@@ -71,15 +51,51 @@ TOKEN2NUM = {
     "z6": 15,
 }
 
+# f(c1, 4) => 9   == +5
+# f(c2, 6) => 9   == +3
+# f(c3, 8) => 9   == +1
+# f(c4, 10) => 9  == -1
 
-# CLEFS = {
-#     "c2": 3,
-# }
+# f(f1, 4) => 5   == +1 
+# f(f2, 6) => 5   == -1
+# f(f3, 8) => 5	  == -3
+# f(f4, 10) => 5  == -5
+
+CLEFS = {
+     "c1": 5,
+     "c2": 3,
+     "c3": 1, 
+     "c4": -1, 
+     "f1": 1, 
+     "f2": -1,
+     "f3": -3,
+     "f4": -5
+ }
+
+# b-Vorzeichen: Ton (=Ton in Volpiano) : b-Vorzeichen in Volpiano
+# B (=b): y 
+# es‘(=e): w 
+# b‘(=j): i 
+# es‘‘(=m): x 
+# b‘‘(=q): z
+
+VOLP2BVOLP = {
+    "b": "y",
+    "e": "w",
+    "j": "i", 
+    "m": "x", 
+    "q": "z"
+}
+
 
 
 # str -> str
 def determine_clef(clef_str):
-    return clef_str
+    if re.search(r"[/\[]", clef_str): #zusammengesetzter clef "c4/f2" oder "[c4]"
+        clef_str = re.search(r"(c\d+|f\d+)", clef_str).group(0)
+        return clef_str
+    else:
+        return clef_str
 
 
 # str -> bool
@@ -90,11 +106,20 @@ def is_note_token(token_str):
 # (str, str) -> str
 def token2volp(clef, token_str):
     if is_note_token(token_str):
-        return NUM2VOLP[TOKEN2NUM[token_str] + 3]
+        return NUM2VOLP[TOKEN2NUM[token_str] + CLEFS[clef]] 
     elif token_str == ",":
         return "-"
     elif token_str == "L":
         return "-3-"
+    elif token_str.startswith("cu_"): # Kustos, in Transkribus: "cu_z2" => 'kleine Note' in Volpiano Großbuchstaben, davor 2 Abstände (lt. RK) (volp: "--A")
+        cu_token_str = token_str.lstrip("cu_") 
+        return "--" + NUM2VOLP[TOKEN2NUM[cu_token_str] + CLEFS[clef]].upper()
+    elif token_str.startswith("b"): # b-Vorzeichen in Transkribus: "bz3"
+        b_token_str = token_str.lstrip("b") 
+        return VOLP2BVOLP[NUM2VOLP[TOKEN2NUM[b_token_str] + CLEFS[clef]]]
+    elif token_str.startswith("n"): #Auflösungszeichen: Großbuchstaben von b-Vorzeichen in Volpiano
+        n_token_str = token_str.lstrip("n")
+        return VOLP2BVOLP[NUM2VOLP[TOKEN2NUM[n_token_str] + CLEFS[clef]]].upper()
     else:
         return "-2-"
 
